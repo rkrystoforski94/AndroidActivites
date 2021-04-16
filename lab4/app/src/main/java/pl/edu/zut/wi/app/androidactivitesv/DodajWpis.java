8d063c860fcd0730643fccbd6cf1e4047c047c19
rkrystoforski94@gmail.com <rkrystoforski94@gmail.com>
2021-04-16 20:14:11 +0200

    Laboratorium 4 - Git, ListView

---
+++ lab4/app/src/main/java/pl/edu/zut/wi/app/androidactivitesv/DodajWpis.java
@@ -0,0 +1,30 @@
+package pl.edu.zut.wi.app.androidactivitesv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class DodajWpis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dodaj_wpis);
+    }
+
+    public void wyslij (View view)
+    {
+        EditText kontrolka =
+                (EditText)findViewById(R.id.editTextTextPersonName);
+        String pole =
+                kontrolka.getText().toString();
+        Intent intencja = new Intent();
+        intencja.putExtra("wpis", pole);
+        setResult(RESULT_OK, intencja);
+        finish();
+    }
+
+}
