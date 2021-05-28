8476bdd62c1e8dbf46d017b9fccb44f0a3074b2c
rkrystoforski94@gmail.com <rkrystoforski94@gmail.com>
2021-05-28 13:52:56 +0200

    Laboratorium 6 Tictoe

---
+++ Lab6/app/src/main/java/com/example/lab6_md42633/HttpService.java
@@ -0,0 +1,95 @@
+package com.example.lab6_md42633;
+
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpService extends IntentService {
+    public static final int GAMES_LIST = 1;
+    public static final int IN_ROW = 2;
+    public static final int REFRESH = 3;
+    public static final int GAME_INFO = 4;
+    public static final String URL = "URL";
+    public static final String METHOD = "Method";
+    public static final String PARAMS = "Params";
+    public static final String RETURN = "Return";
+    public static final String RESPONSE = "Response";
+    public static final String LINES = "http://games.antons.pl/lines/";
+    public static final String XO = "http://games.antons.pl/xo/";
+    public static final int GET = 1;
+    public static final int POST = 2;
+    public static final int PUT = 3;
+
+    //Create constructor
+    public HttpService() {
+        //Parent Constructor
+        super("HTTP calls handler");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        try {
+            //Create url object from given string
+            String urlstr = intent.getStringExtra(HttpService.URL);
+            java.net.URL url = new URL(urlstr);
+            //Prepare connection
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            //set connection method
+            switch (intent.getIntExtra(HttpService.METHOD,1)){
+                case HttpService.POST:
+                    conn.setRequestMethod("POST");
+                    break;
+                case HttpService.PUT:
+                    conn.setRequestMethod("PUT");
+                    break;
+                default:
+                    conn.setRequestMethod("GET");
+            }
+            //Using RSA asynhronic sign for authorization request
+            Config conf = new Config(getApplicationContext());
+            conn.setRequestProperty("PKEY", conf.getPublic().replace("\n",""));
+            conn.setRequestProperty("SIGN", conf.sign(urlstr).replace("\n",""));
+
+            String params = intent.getStringExtra(HttpService.PARAMS);
+            if(params!=null) {
+                conn.setDoOutput(true);
+                OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+                writer.write(params);
+                writer.flush();
+                writer.close();
+            }
+            conn.connect();
+
+            int responseCode = conn.getResponseCode();
+
+            String response = "";
+            if(responseCode==200) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response += line;
+                }
+                reader.close();
+            }
+            conn.disconnect();
+
+            Intent returns = new Intent();
+            returns.putExtra(HttpService.RESPONSE, response);
+            PendingIntent reply = intent.getParcelableExtra(HttpService.RETURN);
+            reply.send(this, responseCode, returns);
+
+        }catch (Exception ex){
+            Log.d("CONNERROR", ex.toString());
+        }
+    }
+}
---
+++ Lab6/app/src/main/java/com/example/lab6_md42633/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.lab6_md42633;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gameList(View v) {
+        Intent intencja = new Intent( getApplicationContext(), GamesList.class);
+        intencja.putExtra("gra", v.getId()); startActivity(intencja); }
+}
